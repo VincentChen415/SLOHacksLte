811c1bf39bbfb90c23a3bee9a201dbc2315c9e45
kaylyn tran <kaylyntran@kaylyns-MacBook-Pro-3.local>
2021-01-23 14:59:03 -0800

    editing tools.js

---
+++ src/tools.js
@@ -0,0 +1,95 @@
+// {
+//     id: '',
+//     name: '',
+//     link: '',
+//     description: '',
+//     tags: [],
+// },
+
+export const tools = [
+    {
+        id: '',
+        name: '',
+        link: '',
+        description: '',
+        tags: [],
+    },
+    {
+        id: '0',
+        name: 'Zoom',
+        link: 'https://www.zoom.us',
+        description: 'Free video conferencing tools for K-12.',
+        tags: ['Communications'],
+    },
+    {
+        id: '1',
+        name: 'RingCentral',
+        link: 'https://www.ringcentral.com/covid19',
+        description: `Free video conferencing for educators (K-12 schools),
+                    health-care provider, or non-profit.`,
+        tags: ['Communications'],
+    },
+    {
+        id: '2',
+        name: 'Google',
+        link: 'https://cloud.google.com/blog/products/g-suite/helping-businesses-and-schools-stay-connected-in-response-to-coronavirus',
+        description: `Free, premium version of its workplace video chat
+                    tool until July, to help businesses and schools working remotely
+                    due to coronavirus. Those features include having up to 250 participants
+                    per call, live streaming for up to 100,000 viewers within a domain, and
+                    the ability to record meetings and save them to Google Drive.`,
+        tags: ['Communications'],
+    },
+    {
+        id: '3',
+        name: 'Slack',
+        link: 'https://slack.com',
+        description: `Free upgrades to paid plans for teams working on coronavirus pandemic
+                    research, response, or mitigation. Interested teams can email a special 
+                    address to get this set up, and a consultation on how best to get started 
+                    with remote collaboration.`,
+        tags: ['Communications'],
+    }
+    {
+        id: '4',
+        name: 'Comcast',
+        link: 'https://wifi.xfinity.com/',
+        description: `Free Xfinity WiFi for everyone, with hotspots available to all, including
+                    non-Xfinity subscribers. To access the service, look for the 
+                    "xfinitywifi" network name in a list of hotspots.`,
+        tags: ['Communications'],
+    },
+    {
+        id: '5',
+        name: 'LogMeIn',
+        link: 'https://blog.gotomeeting.com/coronavirus-disruptions-and-support/',
+        description: `Free site-wide licensing for 3 months of its video conferencing solution, 
+                    GoToMeeting, for eligible organizations (health-care providers, educational
+                    institutions, municipalities and non-profits.)`,
+        tags: ['Communications'],
+    },
+    {
+        id: '6',
+        name: 'Loom',
+        link: 'https://www.loom.com/blog/coronavirus-response',
+        description: `Free video recording and sharing service for teachers and students at K-12
+                    schools, universities, and educational institutions. They have also removed
+                    the recording limit on free plans and have cut the price for Loom Pro in half.`,
+        tags: ['Communications'],
+    },
+    {
+        id: '7',
+        name: 'Intermedia',
+        link: 'https://www.intermedia.net/anymeeting-video-conferencing',
+        description: 'Free Video Conferencing for all of 2020, and Pro Video Conferencing until 2021.',
+        tags: ['Communications'],
+    },
+    {
+        id: '8',
+        name: 'Vonage',
+        link: 'https://www.vonage.com/unified-communications/campaigns/vertical-promo/',
+        description: `Free 3 months of Vonage Business Communications services for educational, healthcare
+                    and non-profit organizations within the US, UK, and Australia.`,
+        tags: ['Communications'],
+    },
+];
