5cb4f40b9c2d639938ff1a064436c4db9bd0eb8f
Vincent Chen <vincentchen@vincents-mbp-3.lan>
2021-01-24 02:27:30 -0800

    finished teams page and did some css

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -7,6 +7,8 @@ function HomePage({}) {
     return(
         <div className="homepage-container">
             <iframe id="human-embed" frameBorder="0" style={{position: "absolute", top: "60px"}} width="100%" height="600" allowFullScreen="true" src="https://human.biodigital.com/viewer?id=production/maleAdult/coronavirus_structure&ui-chapter-list=true&ui-label-list=true&ui-fullscreen=false&ui-all=true&ui-search=false&load-rotate=1&ui-tools=false&ui-info=false&uaid=86wP4"/>
+            <br/>
+            <p>asdasdasd</p> 
         </div>
     );
 };
