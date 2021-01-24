9586a032afd55bd5479fe3874bcd658b2953908a
Enrico <31105303+eNricoC5@users.noreply.github.com>
2021-01-24 03:02:12 -0800

    Mental Page

--- src/pages/MentalPage/MentalPage.js
+++ src/pages/MentalPage/MentalPage.js
@@ -1,11 +1,114 @@
 import './MentalPage.css';
 import React from 'react';
+import covid1 from './covid1.jpg';
+import covid2 from './covid2.jpg';
+import covid3 from './covid3.jpg';
+import covid4 from './covid4.jpg';
+import covid5 from './covid5.jpg';
+import covid6 from './covid6.jpg';
+import covid7 from './covid7.jpg';
+import covid8 from './covid8.jpg';
+
 
 const { Link } = require("react-router-dom");
 
 function MentalPage({}) {
     return(
         <div>
+
+            <br/>
+            <br/>
+            <br/>
+
+            <h1>Mental Health Blog</h1>
+            <p1>Mental health is extremely important pre-covid and is even more so post covid. It’s really easy to get stuck in a ditch from being indoors all day. Whether it’s anxiety of ongoing issues, existential dread and depression, or even just staying indoors too long making you antsy. It's always important to take a few minutes to evaluate yourself and rest. A disclaimer for the following, most these are anecdotal remedies for depression, anxiety, etc and should not be taken as a cure for mental illness. Improving your mental health is a glacial progression and some things don’t work for everyone! There are many resources available and we hope the following as well as our tools page can direct you to receive that support.</p1>
+            
+            <br/>
+            <br/>
+
+            <div>
+            <img className = "Pic1" src = {covid1}></img>
+            </div>
+
+            <br/>
+
+            <h1>Depression Naps and Productivity</h1>
+            <p1>Depression naps are very frequent thing nowadays and can be a bit of a double edged sword. On one hand you get to take a nap yay! But its very easy to ignore your work and also ruin your sleep schedule. A way to combat this is to come up with distractions and try to get a good nights rest and wake up early. Theres nothing wrong with naps as long as you aren’t using them to regain sleep you missed out on. As college students it’s very easy to procrastinate or even stay up for no reason. If productivity is the issue what I have found to help me a lot is making lists not just of the most important things I have to do but also simple stuff. For example, I write “wake up, eat, take a shower”. Having simple stuff that seem doable motivates you to work on the stuff you need to work on. Of course even doing the simple stuff can be a lot on a lot of people and theres no reason to feel bad about yourself for not doing that. Sometimes it is ok to just lay in bed all day. </p1>
+
+            <br/>
+            <br/>
+
+            <div>
+            <img className = "Pic1" src = {covid8}></img>
+            </div>
+
+            <br/>
+
+            <h1>Finding things to do</h1>
+            <p1>As said before a lot of ways to help deter yourself from staring into the abyss is finding distractions. With Covid especially we can’t really go outside as much as we used to or see our friends like we used to. I found having random new hobbies to be super helpful. I’ve tried gardening for the past few weeks and I love it, this doesn’t need to be extravagant or expensive either, having house plants is practically gardening. I love my snake plant because there is barely any sunlight in my RA dorm room and this thing is still alive. I also have been growing green onions. Not only am I saving money on green onions but it's super easy and a low maintenance plant. The best part of staying at home all day is picking up the things I never “had time” now don’t feel bad about yourself if you still don’t do these things but it gives you an idea of stuff to do I have a guitar, a really nice one too, hasn’t felt the touch of a human since high school. Cooking is also great thing not only are you learning valuable life skills but you’re also gonna get healthier meals. Do something different and out of your comfort zone I felt if you make something yourself it’s a little bit easier to eat especially for all my picky eaters out there.</p1>
+            
+            <br/>
+            <br/>
+            
+            <div>
+            <img className = "Pic1" src = {covid2}></img>
+            </div>
+
+            <br/>
+
+            <h1>Exercise</h1>
+            <p1>Exercise is great! I love exercising. I still don’t do it but I always liked the idea of it. The worst part about exercise though is it’s boring. But this shouldn’t stop you from trying something new and getting all the exercise benefits from them. If running isn't your cup of tea try skating, swimming, surfing, or tree climbing. Yoga may look easy but it's also a great form of exercise and the best part is you can do it with your friends virtually. There are loads of resources online to teach you how to do yoga and usually they’re free. Exercising takes a lot out of you but there's something about where I feel better after doing it even if I didn’t do anything productive. It's nice to at least tell myself I exercised today. You may feel silly but its all about having fun and improving your health both physically and mentally.</p1>
+            
+            <br/>
+            <br/>
+           
+            <div>
+            <img className = "Pic1" src = {covid3}></img>
+            </div>
+            
+            <br/>
+            <br/>
+            
+            <div>
+            <img className = "Pic1" src = {covid4}></img>
+            </div>
+
+            <br/>
+ 
+            <h1>Socializing</h1>
+            <p1>Socializing is especially important even if its just meeting up with your therapist on zoom or hopping on zoom/discord/facetime/hangouts with friends. It may be a little different than what we’re used to but its always great to just see people. Sometimes thats all you need, however you do if you play games, play some with friends or do random party style games that don’t need a fancy console or pc. A lot of people have been streaming new shows why not do that with others there's group virtual viewing parties you can do with your favorite streaming sites. It really is all about distracting yourself at least that's what I found to be helpful.</p1>
+            
+            <br/>
+            <br/>
+            
+            <div>
+            <img className = "Pic1" src = {covid7}></img>
+            </div>
+            <br/>
+
+            <h1>Cleaning</h1>
+            <p1>Man this really is something you forget about especially having an episode. Sometimes its just super hard to find the energy to clean but the results always make you feel better. It just the little things to help you get through the day. Doing stuff with my hands honestly helps me whether I’m folding clothes or vacuuming or even reorganizing. You see practically the exact same thing everyday try changing stuff up! Move your bed a certain place or hang up new decorations. Your room really is your safe space and its always nice to invest in your own room and feel comfortable. Hang up the fairy lights no one cares that every college student has them. It gives me serotonin. There is a joy in cleaning and its the smaller things you don’t need to wash all the dishes or clean everything just do whatever you can.</p1>
+            
+            <br/>
+            <br/>
+            
+            <div>
+            <img className = "Pic1" src = {covid5}></img>
+            </div>
+
+            <br/>
+            <h1>Doing you</h1>
+            <p1>The reality of it is there isn't really a sure fire way of coping with exactly what's going on. There's a lot going on in the world and you shouldn't feel obligated to do anything. It's tiring enough just existing at the moment and its good to just take a few minutes and just breathe. Turning off social media and just doing whatever you want to do. </p1>
+            
+            <br/>
+            <br/>
+            
+            <div>
+            <img className = "Pic1" src = {covid6}></img>
+            </div>
+
+            <br/>
+
             <div>MentalPage</div>
         </div>
 
